147d18b71866ff44bd8eb479b7524492c8d5b0af
Jose Ruiz <jruiz793ric@gmail.com>
2024-08-07 21:49:23 -0300

    Mostramos el mensaje en la terminal

--- Server/index.js
+++ Server/index.js
@@ -7,8 +7,15 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', () => {
-    console.log("Tenemos conecion perror")
+io.on('connection', (socket) => {
+    console.log("a user has connected")
+
+    socket.on('disconnect', () => {
+        console.log("an user has disconnected")
+    })
+    socket.on('chat message', (msg) =>{
+        console.log('message: ' + msg)
+    })
 })
 
 app.get(`/`, (req, res) => {
